b3054b8b5ef79fd26d7e5d86a5eda8769ca199df
Bhavya <bkapadia04@gmail.com>
2025-08-16 16:53:23 +0530

    Docker Setup

--- PatientX/billing-service/src/main/java/com/systemdesign/billing_service/grpc/BillingGrpcService.java
+++ PatientX/billing-service/src/main/java/com/systemdesign/billing_service/grpc/BillingGrpcService.java
@@ -1,5 +1,6 @@
 package com.systemdesign.billing_service.grpc;
 import billing.BillingServiceGrpc;
+
 import billing.BillingRequest;
 import billing.BillingResponse;
 import io.grpc.stub.StreamObserver;
@@ -18,10 +19,15 @@ public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBas
     //     responseObserver.onCompleted();
     // }
 
+    private static final org.slf4j.Logger Logger = org.slf4j.LoggerFactory.getLogger(BillingGrpcService.class);
+
     @Override
     public void createBillingAccount(
             BillingRequest request,
             StreamObserver<BillingResponse> responseObserver) {
+
+        Logger.info("Received Billing Request: PatientId={}, Name={}, Email={}",
+                request.getPatientId(), request.getName(), request.getEmail());      
 
         // Example logic: generate an account ID and return success
         BillingResponse response = BillingResponse.newBuilder()
