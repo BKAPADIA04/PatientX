396478fa71e7556f332a5c4a14bb84eb14113679
Bhavya <bkapadia04@gmail.com>
2025-08-16 11:36:32 +0530

    GRPC Setup Done

---
+++ PatientX/billing-service/src/main/java/com/systemdesign/billing_service/grpc/BillingGrpcService.java
@@ -0,0 +1,35 @@
+package com.systemdesign.billing_service.grpc;
+import billing.BillingServiceGrpc;
+import billing.BillingRequest;
+import billing.BillingResponse;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
+    // @Override
+    // public void createBillingAccount(BillingRequest request, StreamObserver<BillingAccountResponse> responseObserver) {
+    //     // Logic to create a billing account
+    //     BillingAccountResponse response = BillingAccountResponse.newBuilder()
+    //             .setAccountId("12345")
+    //             .setStatus("Created")
+    //             .build();
+    //     responseObserver.onNext(response);
+    //     responseObserver.onCompleted();
+    // }
+
+    @Override
+    public void createBillingAccount(
+            BillingRequest request,
+            StreamObserver<BillingResponse> responseObserver) {
+
+        // Example logic: generate an account ID and return success
+        BillingResponse response = BillingResponse.newBuilder()
+                .setAccountId("12345")
+                .setStatus("CREATED")
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
